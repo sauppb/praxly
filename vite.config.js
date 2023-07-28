8d5c32d83fc07acb1d2ce4ab545fe592bf2182c5
Benjamin S <119533034+sauppb@users.noreply.github.com>
2023-07-28 15:51:51 -0400

    fix to prod

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,14 @@
 import { defineConfig } from 'vite'
 
+
+
+const PRAXLY_PATH = process.env.PRAXLY_PATH || "/";
+console.log("\n\n\nPRAXLY_PATH", PRAXLY_PATH, '\n\n\n');
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/praxly/",
+  base: PRAXLY_PATH,
   build: {
-    outDir: 'dist',
+    outDir: "dist",
   },
-  
 });
