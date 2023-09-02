99ce1139efe078402740201e39a8b250d21ccab2
Ben Saupp <bsaupp9999@gmail.com>
2023-09-02 08:36:31 -0400

    future tab work

--- src/main.js
+++ src/main.js
@@ -296,7 +296,36 @@ shareButton.addEventListener('click', generateUrl);
 loadFromUrl(turnCodeToBLocks);
 
 //tab stuff
-function openCity(cityName) {
+// function openCity(cityName) {
+//   // Declare all variables
+//   var i, tabcontent, tablinks;
+
+//   // Get all elements with class="tabcontent" and hide them
+//   tabcontent = document.getElementsByClassName("tabcontent");
+//   for (i = 0; i < tabcontent.length; i++) {
+//     tabcontent[i].style.display = "none";
+//   }
+
+//   // Get all elements with class="tablinks" and remove the class "active"
+//   tablinks = document.getElementsByClassName("tablinks");
+//   for (i = 0; i < tablinks.length; i++) {
+//     tablinks[i].className = tablinks[i].className.replace(" active", "");
+//   }
+
+//   // Show the current tab, and add an "active" class to the button that opened the tab
+//   document.getElementById(cityName).style.display = "block";
+//   // evt.currentTarget.className += " active";
+// } 
+
+// const blocksButton = document.getElementById("blocksButton");
+// const textButton = document.getElementById('textButton');
+// const bothButton = document.getElementById('bothButton');
+// blocksButton.add = openCity('London');
+// textButton.onclick = openCity('Paris');
+// bothButton.onclick = openCity('Tokyo');
+// textButton.onclick = openCity('Paris');
+
+function openCity(evt, cityName) {
   // Declare all variables
   var i, tabcontent, tablinks;
 
@@ -314,14 +343,8 @@ function openCity(cityName) {
 
   // Show the current tab, and add an "active" class to the button that opened the tab
   document.getElementById(cityName).style.display = "block";
-  // evt.currentTarget.className += " active";
+  evt.currentTarget.className += " active";
 } 
-
-const blocksButton = document.getElementById("blocksButton");
-const textButton = document.getElementById('textButton');
-const bothButton = document.getElementById('bothButton');
-blocksButton.onclick = openCity('London');
-textButton.onclick = openCity('Paris');
-bothButton.onclick = openCity('Tokyo')
+// cosnt blocksButton = document.getele
 // end tab stuff
 
